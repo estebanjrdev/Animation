22a18d1fe3ddb0b387db56a61624bc19086b18ec
Esteban Ramos <susoluciones.software@gmail.com>
2024-07-21 22:23:01 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let img = document.getElementById("img");
+
+function funOn() {
+    img.style.animationDuration = "3s";
+}
+
+function funOf() {
+    img.style.animationDuration = "0s";
+}
+
+function fun1() {
+    img.style.animationDuration = "1s";
+}
+
+function fun2() {
+    img.style.animationDuration = "0.5s";
+}
+
+function fun3() {
+    img.style.animationDuration = "0.1s";
+}
